01cb62794a3b5b7497cacedbe1d58d828f9ace42
chrisdugne <chris.dugne@uralys.com>
2018-01-01 22:46:04 +0100

    enhanced component HUDBar

--- cherry/components/hud-bar.lua
+++ cherry/components/hud-bar.lua
@@ -9,56 +9,56 @@ local HUDBar = {}
 --------------------------------------------------------------------------------
 
 function HUDBar:create(options)
-    options = _.defaults(options, {
+    local component = _.defaults(options, {
         height = display.contentHeight*0.07
     })
 
-    if(self.bar) then
-        display.remove(self.bar)
-    end
+    component.height = options.height
+    component.finalX = options.x or display.contentWidth*0.5
+    component.finalY = options.y or component.height*0.5
 
-    self.barHeight = options.height
-
-    self.bar   = display.newGroup()
-    self.bar.x = display.contentWidth*0.5
-    self.bar.y = -self.barHeight*0.5
+    component.display = display.newGroup()
+    component.display.x = component.finalX
+    component.display.y = -component.finalY
 
     if(options.parent) then
-        options.parent:insert(self.bar)
+        options.parent:insert(component.display)
     end
 
-    self.barBG = display.newRect(
-        self.bar,
+    component.bg = display.newRect(
+        component.display,
         0, 0,
         display.contentWidth,
-        self.barHeight
+        component.height
     )
 
-    self.barBG.alpha = 0
-    self.barBG:setFillColor(0)
+    component.bg.alpha = 0
+    component.bg:setFillColor(0)
 
-    self:show()
-    return self
+    setmetatable(component, { __index = HUDBar })
+    component:show()
+
+    return component
 end
 
 --------------------------------------------------------------------------------
 
 function HUDBar:show()
-    transition.to( self.bar, {
+    transition.to( self.display, self.transitionOnShow or {
         time  = 800,
-        y     = self.barHeight*0.5
+        y     = self.finalY
     })
 
-    transition.to( self.barBG, {
+    transition.to( self.bg, {
         time  = 800,
         alpha = 0.6
     })
 end
 
 function HUDBar:hide()
-    transition.to( self.bar, {
+    transition.to( self.display, self.transitionOnHide or {
         time  = 800,
-        y     = -self.barHeight*0.5
+        y     = -self.finalY
     })
 end
 
