d1107530c97606de74930f3c59dbcd290e571eec
chrisdugne <chris.dugne@uralys.com>
2018-01-02 18:22:21 +0100

    reindented components.text

--- cherry/components/text.lua
+++ cherry/components/text.lua
@@ -10,52 +10,52 @@ local Text = {}
 --------------------------------------------------------------------------------
 
 function Text:create(options)
-    local text = _.extend({}, options);
-    setmetatable(text, { __index = Text })
-    text:render()
-    return text
+  local text = _.extend({}, options);
+  setmetatable(text, { __index = Text })
+  text:render()
+  return text
 end
 
 --------------------------------------------------------------------------------
 
 function Text:render()
-    if (self.view) then
-        display.remove(self.view)
-    end
+  if (self.view) then
+    display.remove(self.view)
+  end
 
-    self.view = TextUtils.simple({
-        parent   = self.parent,
-        text     = self.value,
-        font     = self.font or _G.FONT,
-        fontSize = self.fontSize or 55,
-        x        = self.x,
-        y        = self.y
-    })
+  self.view = TextUtils.simple({
+    parent   = self.parent,
+    text     = self.value,
+    font     = self.font or _G.FONT,
+    fontSize = self.fontSize or 55,
+    x        = self.x,
+    y        = self.y
+  })
 
-    self.view:setFillColor( colorize(self.color or 'ffffff') )
+  self.view:setFillColor( colorize(self.color or 'ffffff') )
 
-    if (self.grow) then
-        animation.grow(self.view)
-    end
+  if (self.grow) then
+    animation.grow(self.view)
+  end
 
-    if (self.animation) then
-        transition.to( self.view, self.animation)
-    end
+  if (self.animation) then
+    transition.to( self.view, self.animation)
+  end
 
-    self.view.anchorX = self.anchorX or 0.5
+  self.view.anchorX = self.anchorX or 0.5
 end
 
 --------------------------------------------------------------------------------
 
 function Text:setValue(value)
-    self.value = value
-    self:render()
+  self.value = value
+  self:render()
 end
 
 --------------------------------------------------------------------------------
 
 function Text:destroy()
-    display.remove(self.view)
+  display.remove(self.view)
 end
 
 --------------------------------------------------------------------------------
