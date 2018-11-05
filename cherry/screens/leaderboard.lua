14e035fb7c8234479b7d4e9c6ed81238c15f0eee
chrisdugne <chris.dugne@uralys.com>
2018-11-06 00:11:24 +0100

    added tabs from custom scoreFields and back button

--- cherry/screens/leaderboard.lua
+++ cherry/screens/leaderboard.lua
@@ -1,10 +1,89 @@
 --------------------------------------------------------------------------------
 
-local Button = require 'cherry.components.button'
+local _ = require 'cherry.libs.underscore'
+local gesture = require 'cherry.libs.gesture'
+local Background = require 'cherry.components.background'
 
 --------------------------------------------------------------------------------
 
 local scene = _G.composer.newScene()
+local selectedButton = nil
+
+local CLOSED_Y = 35
+local OPEN_Y = 70
+
+--------------------------------------------------------------------------------
+
+local function select(button)
+  if(selectedButton) then
+    if(button == selectedButton) then return end
+
+    transition.to(selectedButton, {
+      y = CLOSED_Y,
+      time = 250,
+      transition = easing.outBack
+    })
+  end
+
+  selectedButton = button
+
+  transition.to(button, {
+    y = OPEN_Y,
+    time = 250,
+    transition = easing.outBack
+  })
+
+end
+
+local function drawButton(num)
+  local field = _.defaults(App.scoreFields[num], {
+    scale = 1
+  })
+
+  ----------------------
+
+  local button = display.newGroup()
+  App.hud:insert(button)
+
+  button.field = field
+  button.x = 250 + (num - 1) * 150
+  button.y = CLOSED_Y
+
+  ----------------------
+
+  display.newImage(
+    button,
+    'cherry/assets/images/gui/buttons/tab-vertical.png'
+  )
+
+  ----------------------
+
+  local icon = display.newImage(
+    button,
+    field.image,
+    0, 10
+  )
+
+  icon:scale(field.scale, field.scale)
+
+  ----------------------
+
+  display.newText({
+    parent   = button,
+    y        = -50,
+    text     = field.label,
+    font     = _G.FONT,
+    fontSize = 40
+  })
+
+  ----------------------
+
+  gesture.onTap(button, function()
+    select(button)
+  end)
+
+  return button
+end
 
 --------------------------------------------------------------------------------
 
@@ -12,16 +91,29 @@ function scene:create( event )
 end
 
 function scene:show( event )
-    if ( event.phase == 'did' ) then
-        self.leaderboardButton = Button:icon({
-            parent = App.HUD,
-            type   = 'tab',
-            x      = 100,
-            y      = 100,
-            action = function()
-            end
-        })
+  if ( event.phase == 'did' ) then
+    Background:darken()
+
+    local buttons = {}
+    for i = 1, #App.scoreFields do
+      buttons[i] = drawButton(i)
     end
+
+    select(buttons[1])
+
+    local backArrow = display.newImage(
+      App.hud,
+      'cherry/assets/images/gui/items/arrow.right.png',
+      50, 50
+    )
+
+    backArrow.rotation = 180
+    backArrow:scale(0.6, 0.6)
+
+    gesture.onTap(backArrow, function()
+      Router:open(Router.HOME)
+    end)
+  end
 end
 
 function scene:hide( event )
