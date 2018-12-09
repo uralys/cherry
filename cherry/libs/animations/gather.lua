4a79a65f09bae591f22d7c9c96309e16a08b132c
chrisdugne <chris.dugne@uralys.com>
2018-12-09 22:01:56 +0100

    new parent for gathered objects is move toFront to the object are always visible

--- cherry/libs/animations/gather.lua
+++ cherry/libs/animations/gather.lua
@@ -18,6 +18,8 @@ local isArray = require 'cherry.libs.is-array'
 local function gather(movables, options)
   if(not isArray(movables)) then movables = {movables} end
   local newParent = options.newParent
+  newParent:toFront()
+  
   local TIME = options.stepTime or 600
 
   if(options.onComplete) then
