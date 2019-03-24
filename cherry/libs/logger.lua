b279271c59d2b847d5d64f4fa83665f09a14cbed
chrisdugne <chris.dugne@uralys.com>
2019-03-24 15:28:41 +0100

    added prefix for _G.log

--- cherry/libs/logger.lua
+++ cherry/libs/logger.lua
@@ -1,11 +1,16 @@
 local unpack = table.unpack or unpack
 
 _G.log = _G.log or function(stuff, ...)
-  local args = {...}
-  if(type(stuff) == 'table') then
-    local inspect = require 'cherry.libs.inspect'
-    print(inspect(stuff, unpack(args)))
-  else
-    print(stuff, unpack(args))
+    local args = {...}
+    local prefix = ''
+    if (App.name) then
+      prefix = '[' .. App.name .. ']'
+    end
+
+    if (type(stuff) == 'table') then
+      local inspect = require 'cherry.libs.inspect'
+      print(prefix, inspect(stuff, unpack(args)))
+    else
+      print(prefix, stuff, unpack(args))
+    end
   end
-end
