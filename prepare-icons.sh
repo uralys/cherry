ef9fa6d126127685269a88aa63b7286d924001eb
chrisdugne <chris.dugne@uralys.com>
2020-05-05 19:03:00 +0200

    enhanced icons generation script

--- prepare-icons.sh
+++ prepare-icons.sh
@@ -1,24 +1,28 @@
 #!/bin/sh
-iosBase=$PWD/assets/brand/logo-1024-ios.png
-androidBase=$PWD/assets/brand/logo-1024-android.png
-xcassetsPath=$PWD/Images.xcassets/AppIcon.appiconset
-androidPath=$PWD
+iosBase=$PWD/Corona/assets/brand/logo-1024-ios.png
+androidBase=$PWD/Corona/assets/brand/logo-1024-android.png
+
+xcassetsPath=$PWD/Corona/Images.xcassets/AppIcon.appiconset
+androidPath=$PWD/Corona
+
+echo '🍒 Icons generation'
 
 if [ ! -f "$iosBase" ]
 then
-    echo "[error] File $iosBase does not exist"
+    echo "❌ [error] File $iosBase does not exist"
     exit
 fi
 
 if [ ! -f "$androidBase" ]
 then
-    echo "[error] File $androidBase does not exist"
+    echo "❌ [error] File $androidBase does not exist"
     exit
 fi
 
 
 ## https://developer.apple.com/library/archive/documentation/Xcode/Reference/xcode_ref-Asset_Catalog_Format/AppIconType.html
-echo 'creating iOS icons...'
+echo '👨‍🚀 iOS'
+echo '  creating icons...'
 convert "$iosBase" -resize '20x20'      -unsharp 1x4 "$xcassetsPath/Icon-20.png"
 convert "$iosBase" -resize '29x29'      -unsharp 1x4 "$xcassetsPath/Icon-29.png"
 convert "$iosBase" -resize '58x58'      -unsharp 1x4 "$xcassetsPath/Icon-29@2x.png"
@@ -33,16 +37,24 @@ convert "$iosBase" -resize '76x76'      -unsharp 1x4 "$xcassetsPath/Icon-76.png"
 convert "$iosBase" -resize '152x152'    -unsharp 1x4 "$xcassetsPath/Icon-76@2x.png"
 convert "$iosBase" -resize '167x167'    -unsharp 1x4 "$xcassetsPath/Icon-167.png"
 convert "$iosBase" -resize '1024x1024'  -unsharp 1x4 "$xcassetsPath/Icon-1024.png"
-echo "created iOS icons in $xcassetsPath/"
+echo "  ✅ created iOS icons in $xcassetsPath/"
 
 ## -----------------------------------------------------------------------------
-echo 'creating android icons...'
+echo '👨‍🚀 android'
+echo '  creating android icons...'
 convert "$androidBase" -resize '36x36'     -unsharp 1x4 "$androidPath/Icon-ldpi.png"
 convert "$androidBase" -resize '48x48'     -unsharp 1x4 "$androidPath/Icon-mdpi.png"
 convert "$androidBase" -resize '72x72'     -unsharp 1x4 "$androidPath/Icon-hdpi.png"
 convert "$androidBase" -resize '96x96'     -unsharp 1x4 "$androidPath/Icon-xhdpi.png"
 convert "$androidBase" -resize '144x144'   -unsharp 1x4 "$androidPath/Icon-xxhdpi.png"
 convert "$androidBase" -resize '192x192'   -unsharp 1x4 "$androidPath/Icon-xxxhdpi.png"
-echo "created iOS icons in $androidPath/"
+echo '  creating android notification icons...'
+convert "$androidBase" -resize '18x18'     -unsharp 1x4 "$androidPath/IconNotificationDefault-ldpi.png"
+convert "$androidBase" -resize '24x24'     -unsharp 1x4 "$androidPath/IconNotificationDefault-mdpi.png"
+convert "$androidBase" -resize '36x36'      -unsharp 1x4 "$androidPath/IconNotificationDefault-hdpi.png"
+convert "$androidBase" -resize '48x48'     -unsharp 1x4 "$androidPath/IconNotificationDefault-xhdpi.png"
+convert "$androidBase" -resize '72x72'     -unsharp 1x4 "$androidPath/IconNotificationDefault-xxhdpi.png"
+convert "$androidBase" -resize '96x96'     -unsharp 1x4 "$androidPath/IconNotificationDefault-xxxhdpi.png"
+echo "  ✅ created iOS icons in $androidPath/"
 
-echo 'done!'
+echo '✅ done!'
